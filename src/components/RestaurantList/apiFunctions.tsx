13f962bc691e1b411130527051b1f1148b7af04e
Greyson Elkins <greysonelkins@gmail.com>
2021-01-05 15:43:22 -0700

    Write fetch and data cleaning functions

---
+++ src/components/RestaurantList/apiFunctions.tsx
@@ -0,0 +1,36 @@
+import { restaurant, rawRestaurantData } from './definitions'
+
+const cleanRestaurantData = (restaurants: any | rawRestaurantData[]): restaurant[] | string => {
+  if (Array.isArray(restaurants)) {
+    return restaurants.map((
+      { id, name, city, state, telephone, tags, website, genre, attire }
+        ) => {
+          return {
+             id, name, city, state, telephone, tags, website, genre, attire
+          } 
+        })
+  } else {
+    return 'Something went wrong, please try again'
+  }
+}
+
+export const getRestaurants = (): Promise<any | rawRestaurantData[]> => {
+  try {
+    return fetch(
+      `https://code-challenge.spectrumtoolbox.com/api/restaurants`, 
+      {
+        method: 'GET',
+        headers: {
+          "Authorization": "Api-Key q3MNxtfep8Gt"
+        }      
+      })
+        .then(response => {
+          if (response.ok) {
+            const restaurants = response.json()
+            return cleanRestaurantData(restaurants)
+          }
+        })
+  } catch (error) {
+      return error.number
+  }
+}
