1e475f838c1c22d92e1bc8af7301285c5c69c299
Greyson Elkins <greysonelkins@gmail.com>
2021-01-05 19:59:08 -0700

    Update restaurant type to split genres into array

---
+++ src/components/App/definitions.tsx
@@ -0,0 +1,28 @@
+export type rawRestaurantData = {
+  id: string;
+  name: string;
+  address1: string;
+  city: string;
+  state: string;
+  zip: string;
+  lat: string;
+  long: string;
+  telephone: string;
+  tags: string;
+  website: string;
+  genre: string;
+  hours: string;
+  attire: string;
+}
+
+export type restaurant = {
+  id: string;
+  name: string;
+  city: string;
+  state: string;
+  telephone: string;
+  tags: string;
+  website: string;
+  genre: Array<string>;
+  attire: string;
+}
