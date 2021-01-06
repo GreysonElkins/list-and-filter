bc9686361718c6d8ef77da0b2062683516653f20
Greyson Elkins <greysonelkins@gmail.com>
2021-01-05 17:32:47 -0700

    Begin table of restaurants

--- src/components/RestaurantList/RestaurantList.tsx
+++ src/components/RestaurantList/RestaurantList.tsx
@@ -11,9 +11,13 @@ const RestaurantList: React.FC = () => {
   const [restaurantList, setRestaurantList] = useState<restaurant[]>([]) 
   const [error, setError] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [pageNumber, setPageNumber] = useState<number>(1)
 
   useEffect(() => {
        if(isLoading) {
+
+        // still need to sort restaurants, should be called in this block
+
         // getRestaurants()
         //   .then(restaurants => {
         //     if (Array.isArray(restaurants)) {
@@ -32,12 +36,72 @@ const RestaurantList: React.FC = () => {
        }
   })
 
+  const makeRestaurantTable = () => {
+    const restaurantsToDisplay = paginateRestaurantList()
+
+    const listItems = restaurantsToDisplay.map(({ name, city, state, telephone, genre, website }) => {
+      return (
+        <tr>
+          <td>{name}</td>
+          <td>{city}</td>
+          <td>{state}</td>
+          <td>{telephone}</td>
+          <td>{genre}</td>
+          <td>
+            <a href={website} alt={`${name}'s website`}>
+              🌐
+            </a>
+          </td>
+        </tr>
+      )
+    })
+
+    return (
+      <>
+        <table>
+          <thead>
+            <td>Restaurant</td>
+            <td>City</td>
+            <td>State</td>
+            <td>Phone Number</td>
+            <td>Genres</td>
+          </thead>
+          {listItems}
+        </table>
+        <button 
+          disabled={pageNumber > 1 ? false : true}
+          onClick={() => setPageNumber(pageNumber - 1)}
+        >
+          previous
+        </button>
+          {pageNumber}
+        <button
+          // disabled={}
+          onClick={() => setPageNumber(pageNumber + 1)}
+        >
+          next
+        </button>
+      </>
+    )
+  }
+
+  const paginateRestaurantList = () => {
+    const listRange = [(pageNumber - 1) * 10, pageNumber * 10 - 1]
+    let listItems = []
+    
+    for (let i = listRange[0]; i < listRange[1]; i++) {
+      listItems.push(restaurantList[i])
+    }
+
+    return listItems
+  }
+
   return (
     <>
-      <div>Hello List</div>
       <div>{error}</div>
       <SearchAndFilter>
       </SearchAndFilter>
+      {restaurantList.length > 0 && makeRestaurantTable()}
     </>
   )
 }
