6b2824d7ff280a0ecd201d4e44d5f4c57547496e
Greyson Elkins <greysonelkins@gmail.com>
2021-01-06 00:02:04 -0700

    rename list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,94 +1,93 @@
 import React, { useEffect, useState } from 'react'
 
-import SearchAndFilter from '../SearchAndFilter/SearchAndFilter.tsx'
-
 import './RestaurantList.css'
 
 const List: React.FC = () => {
-  const [restaurantList, setRestaurantList] = useState<restaurant[]>([])   
-  const [pageNumber, setPageNumber] = useState<number>(1)
+  // const [restaurantList, setRestaurantList] = useState<restaurant[]>([])   
+  // const [pageNumber, setPageNumber] = useState<number>(1)
 
 
-  const makeRestaurantTable = (page:number) => {
-    const restaurantsToDisplay:restaurant[] = paginateRestaurantList(page)
+  // const makeRestaurantTable = (page:number) => {
+  //   const restaurantsToDisplay:restaurant[] = paginateRestaurantList(page)
 
-    const listItems = restaurantsToDisplay.map(({ name, city, state, telephone, genre, website }) => {
-      return (
-        <tr>
-          <td>{name}</td>
-          <td>{city}</td>
-          <td>{state}</td>
-          <td>{telephone}</td>
-          <td>{genre.join(', ')}</td>
-          <td>
-            <a href={website} title={`${name}'s website`}>
-              🌐
-            </a>
-          </td>
-        </tr>
-      )
-    })
+  //   const listItems = restaurantsToDisplay.map(({ name, city, state, telephone, genre, website }) => {
+  //     return (
+  //       <tr>
+  //         <td>{name}</td>
+  //         <td>{city}</td>
+  //         <td>{state}</td>
+  //         <td>{telephone}</td>
+  //         <td>{genre.join(', ')}</td>
+  //         <td>
+  //           <a href={website} title={`${name}'s website`}>
+  //             🌐
+  //           </a>
+  //         </td>
+  //       </tr>
+  //     )
+  //   })
 
-    return (
-      <>
-        <table>
-          <thead>
-            <td>Restaurant</td>
-            <td>City</td>
-            <td>State</td>
-            <td>Phone Number</td>
-            <td>Genres</td>
-          </thead>
-          {listItems}
-        </table>
-        {makePageNavigation()}
-      </>
-    )
-  }
+  //   return (
+  //     <>
+  //       <table>
+  //         <thead>
+  //           <td>Restaurant</td>
+  //           <td>City</td>
+  //           <td>State</td>
+  //           <td>Phone Number</td>
+  //           <td>Genres</td>
+  //         </thead>
+  //         {listItems}
+  //       </table>
+  //       {makePageNavigation()}
+  //     </>
+  //   )
+  // }
 
-  const makePageNavigation = () => {
-    return (
-      <nav>
-        <button 
-            disabled={pageNumber > 1 ? false : true}
-            onClick={() => setPageNumber(pageNumber - 1)}
-            >
-            previous
-          </button>
-            {pageNumber}
-          <button
-            disabled={paginateRestaurantList(pageNumber + 1).length === 0 ? true : false}
-            onClick={() => setPageNumber(pageNumber + 1)}
-            >
-            next
-          </button>
-          <br />
-          showing 10 out of {restaurantList.length} restaurants
-      </nav>
-    )
-  }
+  // const makePageNavigation = () => {
+  //   return (
+  //     <nav>
+  //       <button 
+  //           disabled={pageNumber > 1 ? false : true}
+  //           onClick={() => setPageNumber(pageNumber - 1)}
+  //           >
+  //           previous
+  //         </button>
+  //           {pageNumber}
+  //         <button
+  //           disabled={paginateRestaurantList(pageNumber + 1).length === 0 ? true : false}
+  //           onClick={() => setPageNumber(pageNumber + 1)}
+  //           >
+  //           next
+  //         </button>
+  //         <br />
+  //         showing 10 out of {restaurantList.length} restaurants
+  //     </nav>
+  //   )
+  // }
 
-  const paginateRestaurantList = (page:number): restaurant[] => {
-    const listRange = [(page - 1) * 10, page * 10 - 1]
-    let listItems = []
+  // const paginateRestaurantList = (page:number): restaurant[] => {
+  //   const listRange = [(page - 1) * 10, page * 10 - 1]
+  //   let listItems = []
     
-    for (let i = listRange[0]; i < listRange[1]; i++) {
-      if (restaurantList[i]) listItems.push(restaurantList[i])
-    }
+  //   for (let i = listRange[0]; i < listRange[1]; i++) {
+  //     if (restaurantList[i]) listItems.push(restaurantList[i])
+  //   }
 
-    return listItems
-  }
+  //   return listItems
+  // }
 
   return (
     <>
-      <div>{error}</div>
+    Hello List
+      {/* <div>{error}</div>
       {restaurantList.length > 0 && 
         <>
           {makeRestaurantTable(pageNumber)}
           <SearchAndFilter>
           </SearchAndFilter>
         </>
-      }
+      } */}
     </>
   )
 }
